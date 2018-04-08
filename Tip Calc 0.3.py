6071ec252162ef3d23230c546afc309c9354abbb
JoshuaM928 <38165111+JoshuaM928@users.noreply.github.com>
2018-04-08 13:18:12 -0700

    Add files via upload
    changed the cost var to 'X' so that all you have to do is input your cost into it

---
+++ Tip Calc 0.3.py
@@ -0,0 +1,7 @@
+cost = X               #Input Cost into "X"
+tax = 7.25/100         #CA state tax is 7.25%
+tip = 16.3/100         #A good is tip 16.3%
+
+cost = tax * cost + cost
+total = tip * cost + cost
+print (total)
